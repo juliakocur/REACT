cdc4dc0426746b57612e366b52166cf03905c41a
Julia Kocur <juliakocur@mail.com>
2023-10-19 11:03:07 +0200

    feat: add test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
-    'airbnb',
-    'airbnb/hooks',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
@@ -26,7 +24,7 @@ module.exports = {
   plugins: ['@typescript-eslint', 'react', 'prettier', 'react-hooks'],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/exhaustive-deps': 'off',
     'comma-dangle': ['error', 'only-multiline'],
     'react/prop-types': 'off',
     'react/display-name': 'off',
@@ -38,10 +36,9 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-var-reqiures': 'off',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
   },
   settings: {
     react: {
@@ -49,4 +46,5 @@ module.exports = {
       version: 'detect',
     },
   },
+  ignorePatterns: ['vite.config.ts'],
 };
