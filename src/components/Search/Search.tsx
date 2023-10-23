cb300543209a99cdcc0063bfb68cd72fb06cfeeb
Julia Kocur <juliakocur@mail.com>
2023-10-23 10:57:36 +0200

    feat: add data to LocalStorage

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,11 +4,30 @@ import logo from '../../assets/logo.png';
 import './Search.css';
 
 class Search extends React.Component {
+  state = {
+    value: localStorage.getItem('Value') ?? '',
+  };
+
+  componentWillUnmount(): void {
+    localStorage.setItem('Value', this.state.value);
+  }
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ value: event.target.value });
+    localStorage.setItem('Value', event.target.value);
+  };
+
   render() {
     return (
       <div className="search">
         <img src={logo} className="logo" alt="logo"></img>
-        <input className="search__field" type="text" placeholder="search ..." />
+        <input
+          className="search__field"
+          type="text"
+          placeholder="search ..."
+          value={this.state.value}
+          onInput={this.handleChange}
+        />
         <img src={search} className="search__icon" alt="icon"></img>
       </div>
     );
