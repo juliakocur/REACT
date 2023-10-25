c0799bc4ab0e8ee15effbd34ecbf1843cdc675d8
Julia Kocur <juliakocur@mail.com>
2023-10-25 21:28:31 +0200

    fix: run eslint

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -11,18 +11,10 @@ class Cards extends React.Component<StarShips> {
       <div className="cards">
         <h3 className="cards__header">{this.props.name}</h3>
         <p className="cards__description">Manufacturer: <span>{this.props.manufacturer}</span></p>
-        <p className="cards__description">
-          Model: <span>{this.props.model}</span>
-        </p>
-        <p className="cards__description">
-          Class: <span>{this.props.starship_class}</span>
-        </p>
-        <p className="cards__description">
-          Passengers: <span>{this.props.passengers}</span>
-        </p>
-        <p className="cards__description">
-          Length: <span>{this.props.length} m</span>
-        </p>
+        <p className="cards__description">Model: <span>{this.props.model}</span></p>
+        <p className="cards__description">Class: <span>{this.props.starship_class}</span></p>
+        <p className="cards__description">Passengers: <span>{this.props.passengers}</span></p>
+        <p className="cards__description">Length: <span>{this.props.length} m</span></p>
       </div>
     );
   }
