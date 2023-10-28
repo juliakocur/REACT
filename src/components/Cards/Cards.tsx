c95c37e600ae425a84661e3387d6ab44cb19a0b6
Julia Kocur <juliakocur@mail.com>
2023-10-28 11:21:01 +0200

    refactor: use hooks for API, Cards, Loader and ErrorButton

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,33 +1,27 @@
-import React from 'react';
 import './Cards.css';
 import { IShips as StarShips } from '../Types/index';
 
-class Cards extends React.Component<StarShips> {
-  constructor(props: StarShips) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="cards">
-        <h3 className="cards__header">{this.props.name}</h3>
-        <p className="cards__description">
-          Manufacturer: <span>{this.props.manufacturer}</span>
-        </p>
-        <p className="cards__description">
-          Model: <span>{this.props.model}</span>
-        </p>
-        <p className="cards__description">
-          Class: <span>{this.props.starship_class}</span>
-        </p>
-        <p className="cards__description">
-          Passengers: <span>{this.props.passengers}</span>
-        </p>
-        <p className="cards__description">
-          Length: <span>{this.props.length} m</span>
-        </p>
-      </div>
-    );
-  }
-}
+const Cards = (props: StarShips) => {
+  return (
+    <div className="cards">
+      <h3 className="cards__header">{props.name}</h3>
+      <p className="cards__description">
+        Manufacturer: <span>{props.manufacturer}</span>
+      </p>
+      <p className="cards__description">
+        Model: <span>{props.model}</span>
+      </p>
+      <p className="cards__description">
+        Class: <span>{props.starship_class}</span>
+      </p>
+      <p className="cards__description">
+        Passengers: <span>{props.passengers}</span>
+      </p>
+      <p className="cards__description">
+        Length: <span>{props.length} m</span>
+      </p>
+    </div>
+  );
+};
 
 export default Cards;
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
 import './Loader.css';
 
-class Loader extends React.Component {
-  render() {
-    return <div className="loader"></div>;
-  }
-}
+const Loader = () => {
+  return <div className="loader"></div>;
+};
 
 export default Loader;
